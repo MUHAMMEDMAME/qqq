3e522e200c171a092f2bcf0579cd576449c5f53b
Muhammed Nour <118080466+MUHAMMEDMAME@users.noreply.github.com>
2024-03-23 02:20:21 +0300

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	unsigned int n = 1;
+	cout << "N" << "\t" << "10*N"<<"\t" << "100*N" << "\t" << "1000*N" << endl;
+	while (n<=5) {
+		cout << n << "\t" << 10 * n << "\t" << 100 * n <<"\t"<< 1000 * n << endl;
+		n++;
+	}
+
+
+
+}
